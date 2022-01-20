833f87587bba974ff40b0bfc96193bd794cc0d1f
James Wilson <jw@jameswilson.io>
2022-01-20 19:54:03 +1100

    refactored to be a function

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { App, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { DNSStack } from "./dns-stack";
 import { ECRStack } from "./ecr-stack";
@@ -8,118 +8,111 @@ import { RDSStack } from "./rds-stack";
 import { SecretsStack } from "./secrets-stack";
 import { VPCStack } from "./vpc-stack";
 import * as cdk from "aws-cdk-lib";
+import { ClusterConfig } from "./config";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export interface JK8SAwsCdkProps extends StackProps {
-  // Define construct properties here
-  name: string;
-  fqdn: string;
-}
+export function deployCluster(
+  cdkApp: App,
+  name: string,
+  config: ClusterConfig
+) {
+  const vpc = new VPCStack(cdkApp, "VPCStack", {});
 
-export class JK8SAwsCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props: JK8SAwsCdkProps) {
-    super(scope, id);
+  const secrets = new SecretsStack(cdkApp, "SecretsStack", {});
 
-    const { name, fqdn } = props;
+  const dns = new DNSStack(cdkApp, "DNSStack", {
+    fqdn: config.fqdn,
+  });
 
-    const vpc = new VPCStack(this, "VPCStack", {});
+  const rds = new RDSStack(cdkApp, "RDSStack", {
+    vpc,
+  });
 
-    const secrets = new SecretsStack(this, "SecretsStack", {});
+  const opensearch = new OpenSearchStack(cdkApp, "OpenSearchStack", { vpc });
 
-    const dns = new DNSStack(this, "DNSStack", {
-      fqdn,
-    });
+  const cluster = new EKSStack(cdkApp, "EKSStack", {
+    name,
+    vpc,
+    rds,
+    opensearch,
+  });
 
-    const rds = new RDSStack(this, "RDSStack", {
-      vpc,
-    });
+  const registry = new ECRStack(cdkApp, "ECRStack", {});
 
-    const opensearch = new OpenSearchStack(this, "OpenSearchStack", { vpc });
+  new cdk.CfnOutput(cdkApp, "ClusterDNSPolicyARN", {
+    value: dns.clusterDNSPolicy.managedPolicyArn,
+    description: "Policy for in-cluster manipulation of Route53",
+    exportName: "ClusterDNSPolicyARN",
+  });
+  new cdk.CfnOutput(cdkApp, "FQDN", {
+    value: dns.zone.zoneName,
+    description: "FQDN",
+    exportName: "FQDN",
+  });
+  new cdk.CfnOutput(cdkApp, "ZoneID", {
+    value: dns.zone.hostedZoneId,
+    description: "ZoneID",
+    exportName: "ZoneID",
+  });
 
-    const cluster = new EKSStack(this, "EKSStack", {
-      name,
-      vpc,
-      rds,
-      opensearch,
-    });
+  new cdk.CfnOutput(cdkApp, "ArgoCDSecretsPolicyARN", {
+    value: secrets.argocdSecretsPolicy.managedPolicyArn,
+    description: "Policy for ArgoCD Secrets Retrieval",
+    exportName: "ArgoCDSecretsPolicyARN",
+  });
+  new cdk.CfnOutput(cdkApp, "TelemetrySecretsPolicyARN", {
+    value: secrets.telemetrySecretsPolicy.managedPolicyArn,
+    description: "Policy for Telemetry Secrets Retrieval",
+    exportName: "TelemetrySecretsPolicyARN",
+  });
+  new cdk.CfnOutput(cdkApp, "JaegerSecretsPolicyARN", {
+    value: secrets.jaegerSecretsPolicy.managedPolicyArn,
+    description: "Policy for Jaeger Secrets Retrieval",
+    exportName: "JaegerSecretsPolicyARN",
+  });
+  new cdk.CfnOutput(cdkApp, "AppSecretsPolicyARN", {
+    value: secrets.appSecretsPolicy.managedPolicyArn,
+    description: "Policy for App Secrets Retrieval",
+    exportName: "AppSecretsPolicyARN",
+  });
 
-    const registry = new ECRStack(this, "ECRStack", {});
+  new cdk.CfnOutput(cdkApp, "RDSHost", {
+    value: rds.db.instanceEndpoint.hostname,
+  });
+  new cdk.CfnOutput(cdkApp, "RDSSecretName", {
+    value: rds.db.secret?.secretName!,
+  });
 
-    new cdk.CfnOutput(this, "ClusterDNSPolicyARN", {
-      value: dns.clusterDNSPolicy.managedPolicyArn,
-      description: "Policy for in-cluster manipulation of Route53",
-      exportName: "ClusterDNSPolicyARN",
-    });
-    new cdk.CfnOutput(this, "FQDN", {
-      value: dns.zone.zoneName,
-      description: "FQDN",
-      exportName: "FQDN",
-    });
-    new cdk.CfnOutput(this, "ZoneID", {
-      value: dns.zone.hostedZoneId,
-      description: "ZoneID",
-      exportName: "ZoneID",
-    });
+  new cdk.CfnOutput(cdkApp, "ClusterARN", {
+    value: cluster.cluster.clusterArn,
+    description: "Cluster ARN",
+    exportName: "ClusterARN",
+  });
+  new cdk.CfnOutput(cdkApp, "ClusterName", {
+    value: cluster.cluster.clusterName,
+    description: "Cluster Name",
+    exportName: "ClusterName",
+  });
 
-    new cdk.CfnOutput(this, "ArgoCDSecretsPolicyARN", {
-      value: secrets.argocdSecretsPolicy.managedPolicyArn,
-      description: "Policy for ArgoCD Secrets Retrieval",
-      exportName: "ArgoCDSecretsPolicyARN",
-    });
-    new cdk.CfnOutput(this, "TelemetrySecretsPolicyARN", {
-      value: secrets.telemetrySecretsPolicy.managedPolicyArn,
-      description: "Policy for Telemetry Secrets Retrieval",
-      exportName: "TelemetrySecretsPolicyARN",
-    });
-    new cdk.CfnOutput(this, "JaegerSecretsPolicyARN", {
-      value: secrets.jaegerSecretsPolicy.managedPolicyArn,
-      description: "Policy for Jaeger Secrets Retrieval",
-      exportName: "JaegerSecretsPolicyARN",
-    });
-    new cdk.CfnOutput(this, "AppSecretsPolicyARN", {
-      value: secrets.appSecretsPolicy.managedPolicyArn,
-      description: "Policy for App Secrets Retrieval",
-      exportName: "AppSecretsPolicyARN",
-    });
+  new cdk.CfnOutput(cdkApp, "ClusterStackName", {
+    value: cluster.stackName,
+    description: "Cluster Stack Name",
+    exportName: "ClusterStackName",
+  });
 
-    new cdk.CfnOutput(this, "RDSHost", {
-      value: rds.db.instanceEndpoint.hostname,
-    });
-    new cdk.CfnOutput(this, "RDSSecretName", {
-      value: rds.db.secret?.secretName!,
-    });
-
-    new cdk.CfnOutput(this, "ClusterARN", {
-      value: cluster.cluster.clusterArn,
-      description: "Cluster ARN",
-      exportName: "ClusterARN",
-    });
-    new cdk.CfnOutput(this, "ClusterName", {
-      value: cluster.cluster.clusterName,
-      description: "Cluster Name",
-      exportName: "ClusterName",
-    });
-
-    new cdk.CfnOutput(this, "ClusterStackName", {
-      value: cluster.stackName,
-      description: "Cluster Stack Name",
-      exportName: "ClusterStackName",
-    });
-
-    new cdk.CfnOutput(this, "OpenSearchDomain", {
-      value: opensearch.domain.domainEndpoint,
-      description: "OpenSearch Domain",
-      exportName: "OpenSearchDomain",
-    });
-    new cdk.CfnOutput(this, "MasterUserSecretName", {
-      value: opensearch.domain.masterUserPassword?.toString()!,
-      description: "OpenSearch Master User Secret Name",
-      exportName: "MasterUserSecretName",
-    });
-    new cdk.CfnOutput(this, "MasterUserGeneratedPassword", {
-      value: opensearch.domain.masterUserPassword?.toString()!,
-      description: "OpenSearch Master User Generated Password",
-      exportName: "MasterUserGeneratedPassword",
-    });
-  }
+  new cdk.CfnOutput(cdkApp, "OpenSearchDomain", {
+    value: opensearch.domain.domainEndpoint,
+    description: "OpenSearch Domain",
+    exportName: "OpenSearchDomain",
+  });
+  new cdk.CfnOutput(cdkApp, "MasterUserSecretName", {
+    value: opensearch.domain.masterUserPassword?.toString()!,
+    description: "OpenSearch Master User Secret Name",
+    exportName: "MasterUserSecretName",
+  });
+  new cdk.CfnOutput(cdkApp, "MasterUserGeneratedPassword", {
+    value: opensearch.domain.masterUserPassword?.toString()!,
+    description: "OpenSearch Master User Generated Password",
+    exportName: "MasterUserGeneratedPassword",
+  });
 }
